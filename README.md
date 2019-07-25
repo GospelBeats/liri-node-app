67f8d43ff5a411f3fde0adecd3d65ba620244814
Cory <cyb12345@comcast.net>
2019-07-25 18:07:16 -0400

    completed app

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# liri-node-app
+liri-node-app
+
+Creator: Cory Walker
+Created on: 7-25-2019
+
+About The App
+
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+HOW TO USE LIRI
+
+Step by Step instructions: 
+Open your terminal, command line or git bash.
+
+Navigate to the folder that contains the liri.js file.
+
+Depending on the command you run, the output will vary.
+
+Example 1: Run the "concert-this" command
+node liri.js concert-this <name of artist or band>
+
+Output: The system will display a list of all events and locations where the artist or band will perform. It includes the name, country, region, city, date, and time. It can result in multiple records. 
+
+Example 2: Run the "spotify-this-song" command
+node liri.js spotify-this-song <name of song>
+
+Output: The system will display a list of information associated with the song. It includes song name, url to preview the song, album, and artist name. It can result in multiple records. 
+
+Example 3: Run the "movie-this" command
+node liri.js movie-this <name of movie>
+
+Output: The system will display information associated with the movie. This information includes the movie title, release year, rating, country of production, language, plot, and actors.
+
+Example 4: Run the "do-what-it-says" command
+node liri.js do-what-it-says
+
+Output: The system will read the text in the random.txt file, and perform the commands listed in the random.txt file.
+
+TECHNOLOGIES USED
+Javascript
+Nodejs
+Node packages:
+Node-Spotify-API
+Axios
+Moment
+DotEnv
+APIs used:
+Bands in Town
+OMDB
+Spotify
--- liri.js
+++ liri.js
@@ -1,17 +1,20 @@
+
+// required variables used in the functions
 require("dotenv").config();
-
 const axios = require('axios');
-var Spotify = require('node-spotify-api');
-
+var fs = require("fs");
+const Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
 
+// variables to capture user input in the command line
 let cmd = process.argv[2];
-let arg = process.argv[3];//.slice(3).join(" ");
+let arg = process.argv[3];
 
-console.log(arg);
-
+// execute function with user input 
 input(cmd, arg);
 
+// switch statement that determines which function to call based on user input
 function input(cmd, arg){
   switch (cmd) {
   case 'concert-this':
@@ -26,19 +29,20 @@ function input(cmd, arg){
   case 'do-what-it-says':
       someInfo();
       break;
-  default: 
+  default: // error log if user input is not correct
       console.log("Please type one of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says");
   }
 }
 
-//Funtion for Concert Info: Bands in Town
+// funtion for Concert Info, Bands in Town
 function concertInfo(arg){
   axios.get("https://rest.bandsintown.com/artists/" + arg + "/events?app_id=codingbootcamp")
   .then(function (response) {
-    // handle success
+
+    // returned api data stored in variable
     let concerts = response.data;
-    
-    
+
+    // looping thru the data, printing the results
     for (let i = 0; i < concerts.length; i++){
       console.log("\n");
       console.log("*************** EVENT INFO ****************\n");
@@ -46,42 +50,95 @@ function concertInfo(arg){
       console.log("Venue Country: " + concerts[i].venue.country + "\n");
       console.log("Venue Region: " + concerts[i].venue.region + "\n");
       console.log("Venue City: " + concerts[i].venue.city + "\n");
-      console.log("Venue Date/Time: " + concerts[i].date + "\n");
+      console.log("Venue Date/Time: " + concerts[i].datetime + "\n");
       console.log("*************** EVENT INFO ****************\n");
       console.log("\n");
     }
    
   })
   .catch(function (error) {
-    // handle error
+    // error message if something went wrong
     console.log(error);
   })
 }
 
-// if (cmd === "concert-this") {
+// funtion for Spotify Music Info 
+function songInfo(arg) {
+  if (arg === undefined) {
+      arg = "The Sign"; //default Song
+  }
+  // data for spotify to search
+  spotify.search(
+      {
+          type: "track",
+          query: arg
+      }, // function that catch the error
+      function (err, data) {
+          if (err) {
+              console.log("Error occurred: " + err);
+              return;
+          }
+          // returned api data stored in variable
+          var songs = data.tracks.items;
 
-//     axios.get("https://rest.bandsintown.com/artists/" + arg + "/events?app_id=codingbootcamp")
-//     .then(function (response) {
-//       // handle success
-//       let concerts = response.data;
-//       //console.log(concerts[0]);
+          // looping thru the data, printing the results
+          for (var i = 0; i < songs.length; i++) {
+              console.log("**********SONG INFO*********");
+              console.log(i);
+              console.log("Song name: " + songs[i].name);
+              console.log("Preview song: " + songs[i].preview_url);
+              console.log("Album: " + songs[i].album.name);
+              console.log("Artist(s): " + songs[i].artists[0].name);
+              console.log("*****************************");  
+             
+           }
+      }
+  );
+};
 
-//       for (let i = 0; i < concerts.length; i++){
-//         console.log("\n");
-//         console.log(concerts[i].venue.name);
-//         console.log(concerts[i].venue.country);
-//         console.log(concerts[i].venue.region);
-//         console.log(concerts[i].venue.city);
-//         console.log(concerts[i].datetime);
-//       }
-     
-//     })
-//     .catch(function (error) {
-//       // handle error
-//       console.log(error);
-//     })  
-// } else if (cmd === "spotify-this") {
-//     console.log("this is test");
-// }
+// funtion for OMDB Movie Info
+// if user did not specify a movie, print this by default
+function movieInfo(arg){
+  if (arg === undefined) {
+      arg = "Mr. Nobody"
+      console.log("-----------------------"); 
+      console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");  
+      console.log("It's on Netflix!");
+  }
+  // api call to get movie data
+  axios.get("http://www.omdbapi.com/?t=" + arg + "&y=&plot=short&apikey=b3c0b435")
+  .then(function (response) {
 
-// console.log(cmd, arg);
+    // returned api data stored in variable
+    let movies = response.data;
+    
+    // looping thru the data, printing the results
+    console.log("\n");
+    console.log("**********MOVIE INFO*********");    
+    console.log("Title: " + movies.Title);
+    console.log("Release Year: " + movies.Year);
+    console.log("IMDB Rating: " + movies.imdbRating); 
+    console.log("Rotten Tomatoes Rating: " + JSON.stringify(movies.Ratings));
+    console.log("Country of Production: " + movies.Country);
+    console.log("Language: " + movies.Language); 
+    console.log("Plot: " + movies.Plot);
+    console.log("Actors: " + movies.Actors);
+    console.log("*****************************"); 
+    console.log("\n");
+   
+  })
+  .catch(function (error) {
+    // error message if something went wrong
+    console.log(error);
+  })
+}
+// function to read the random.txt file
+function someInfo(){
+	fs.readFile('random.txt', 'utf8', function(err, data){
+		if (err){ 
+			return console.log(err);
+		}   // arranges the data layout
+        var dataArr = data.split(',');
+        input(dataArr[0], dataArr[1]);
+	});
+}
